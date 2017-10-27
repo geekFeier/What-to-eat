03b9abc6c576900fca9dbfe3fe94fd44e1391bd6
YXY <xinyueyang1204@163.com>
2017-10-27 16:33:47 +0800

    小优化

--- js/food.js
+++ js/food.js
@@ -1,133 +1,148 @@
-var foodBreakfast = [],
-	foodLunch = [],
-	foodSupper = [];
-// 重复使用的dom
-var oStop = document.getElementById('stop'),
-	oMoodMessage = document.getElementById('mood-message'),
-	oWrapperInner = document.getElementById('wrapper-inner'),
-	oConfirmMsg = document.getElementById('confirm-message'),
-	oConfirm = document.getElementById('confirm'),
-	oMoodSad = document.getElementById('mood-sad'),
-	oMoodHappy = document.getElementById('mood-happy'),
-	oConfirmNo = document.getElementById('confirm-no'),
-	oConfirmYes = document.getElementById('confirm-yes'),
-	oMoodBtn = document.getElementsByClassName('mood-btn'),
-	oFoodName = document.getElementById('food-name'),
-	oFoodName2 = document.getElementById('food-name2'),
-	oFood = document.getElementById('food'),
-	oUl = document.getElementsByTagName('ul')[0],
-	oLi = document.getElementsByTagName('li'),
-	oBtns = document.getElementsByClassName('button');
+	var foodBreakfast = foodLunch = foodSupper = [];
 
-function loadFood() {
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET", "../api/GetFood.php", true);
-	xmlhttp.send();
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			xmlDoc = JSON.parse(xmlhttp.responseText);
-			[foodBreakfast, foodLunch, foodSupper] = [xmlDoc.breakfast, xmlDoc.lunch, xmlDoc.dinner];
-			var timeTxtObj = [], // 早中晚的单词
-				date = new Date(),
-				hour = date.getHours(),
-				foodBtnObj = document.getElementsByClassName('food-btn'),
-				flag = true;
-			//在页面中动态添加菜名
-			var appendCon = function(timeTxt) { //timeTxt：点击的文字  
-				oFood.innerHTML = '';
-				var timeFoodIndex = timeTxtObj.indexOf(timeTxt), //当前点击的btn的索引
-					timeFoodThis = [foodBreakfast, foodLunch, foodSupper][timeFoodIndex];
-				for (var i in timeFoodThis) {
-					oFood.innerHTML += '<li>' + timeFoodThis[i] + '</li>'
-				}
-				oUl.innerHTML = oUl.innerHTML + oUl.innerHTML;
-				oUl.style.height = oLi[0].offsetHeight * oLi.length + 'px';
-				timer = setInterval(move, 50);
-			}
+	var getId = (function(func) {
+		return function() {
+			return func.apply(document, arguments);
+		}
+	})(document.getElementById);
+
+	var getClass = (function(func) {
+		return function() {
+			return func.apply(document, arguments);
+		}
+	})(document.getElementsByClassName);
+
+	var getTagName = (function(func) {
+		return function() {
+			return func.apply(document, arguments);
+		}
+	})(document.getElementsByTagName);
 
 
-			// 点击早中午btn
-			for (var i = 0, len = foodBtnObj.length; i < len; i++) {
-				timeTxtObj.push(foodBtnObj[i].innerHTML);
-				foodBtnObj[i].onclick = function() {
-					switch (this.innerHTML) {
-						case timeTxtObj[0]:
-							showNode(foodBreakfast, 0)
-							break;
-						case timeTxtObj[1]:
-							showNode(foodLunch, 1)
-							break;
-						case timeTxtObj[2]:
-							showNode(foodSupper, 2)
-							break;
-						default:
-							break;
+	var oMoodMessage = getId('mood-message'),
+		oWrapperInner = getId('wrapper-inner'),
+		oConfirmMsg = getId('confirm-message'),
+		oConfirm = getId('confirm'),
+		oMoodSad = getId('mood-sad'),
+		oMoodHappy = getId('mood-happy'),
+		oMoodBtn = getClass('mood-btn'),
+		oFoodName = getId('food-name'),
+		oFoodName2 = getId('food-name2'),
+		oFood = getId('food'),
+		oUl = getTagName('ul')[0],
+		oLi = getTagName('li'),
+		oBtns = getClass('button');
+
+	function loadFood() {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "../api/GetFood.php", true);
+		// xmlhttp.send();
+		xmlhttp.onreadystatechange = function() {
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				xmlDoc = JSON.parse(xmlhttp.responseText);
+				[foodBreakfast, foodLunch, foodSupper] = [xmlDoc.breakfast, xmlDoc.lunch, xmlDoc.dinner];
+				var timeTxtObj = [], // 早中晚的单词
+					date = new Date(),
+					hour = date.getHours(),
+					foodBtnObj = getClass('food-btn'),
+					flag = true;
+				//在页面中动态添加菜名
+				var appendCon = function(timeTxt) { //timeTxt：点击的文字  
+					oFood.innerHTML = '';
+					var timeFoodIndex = timeTxtObj.indexOf(timeTxt), //当前点击的btn的索引
+						timeFoodThis = [foodBreakfast, foodLunch, foodSupper][timeFoodIndex];
+					for (var i in timeFoodThis) {
+						oFood.innerHTML += '<li>' + timeFoodThis[i] + '</li>'
 					}
+					oUl.innerHTML = oUl.innerHTML + oUl.innerHTML;
+					oUl.style.height = oLi[0].offsetHeight * oLi.length + 'px';
+					timer = setInterval(move, 50);
+				}
 
-					function showNode(foodArr, i) {
-						if (foodArr.length) {
-							if (flag) {
-								foodBtnObj[i].parentNode.className = 'sim-button button btn-on'
-								var timeTxt = foodBtnObj[i].innerHTML;
-								appendCon(timeTxt);
-								document.getElementById('stop').style.display = 'block';
-								flag = false;
+
+				// 点击早中午btn
+				for (var i = 0, len = foodBtnObj.length; i < len; i++) {
+					timeTxtObj.push(foodBtnObj[i].innerHTML);
+					foodBtnObj[i].onclick = function() {
+						switch (this.innerHTML) {
+							case timeTxtObj[0]:
+								showNode(foodBreakfast, 0)
+								break;
+							case timeTxtObj[1]:
+								showNode(foodLunch, 1)
+								break;
+							case timeTxtObj[2]:
+								showNode(foodSupper, 2)
+								break;
+							default:
+								break;
+						}
+
+						function showNode(foodArr, i) {
+							if (foodArr.length) {
+								if (flag) {
+									foodBtnObj[i].parentNode.className = 'sim-button button btn-on'
+									var timeTxt = foodBtnObj[i].innerHTML;
+									appendCon(timeTxt);
+									getId('stop').style.display = 'block';
+									flag = false;
+								} else {
+									alert("正在摇啊摇，请先Duang！")
+								}
 							} else {
-								alert("正在摇啊摇，请先Duang！")
+								alert(timeTxtObj[i] + " 还未准备好，你可以手动添加哦")
 							}
-						} else {
-							alert(timeTxtObj[i] + " 还未准备好，你可以手动添加哦")
 						}
 					}
 				}
-			}
-			//无缝滚动菜单
-			function move() {
-				var oUlHeightAbs = Math.abs(oUl.offsetTop),
-					oUlH = oUl.offsetHeight / 2;
-				if (oUlHeightAbs > oUlH) {
-					oUl.style.top = '0';
+				//无缝滚动菜单
+				function move() {
+					var oUlHeightAbs = Math.abs(oUl.offsetTop),
+						oUlH = oUl.offsetHeight / 2;
+					if (oUlHeightAbs > oUlH) {
+						oUl.style.top = '0';
+					}
+					oUl.style.top = oUl.offsetTop + (-45) + 'px';
 				}
-				oUl.style.top = oUl.offsetTop + (-45) + 'px';
-			}
 
+				document.addEventListener('click', function(e) {
+					switch (e.target.id) {
+						case 'stop':
+							for (var i = 0, len = foodBtnObj.length; i < len; i++) {
+								oBtns[i].className = 'sim-button button';
+							}
+							window.clearInterval(timer);
+							flag = true;
+							var index = (parseInt(oFood.style.top) / 45 - 1).toString().slice(1);
+							foodNameTxt = oLi[index - 1].innerHTML;
+							oConfirmMsginnerHTML = '咱今天就吃"' + foodNameTxt + '"???';
+							oWrapperInner.style.display = 'none';
+							oConfirm.style.display = 'block';
+							oFoodName2.innerHTML = foodNameTxt;
+							break;
+						case 'confirm-no':
+							oMoodMessage.innerHTML = foodNameTxt + " 已黯然失色躲在角落检测身体营养指数哭到妆都花了还是会深思熟虑如何才能博君子一笑即使还是会痛定思痛。";
+							oWrapperInner.style.display = 'none';
+							oConfirm.style.display = 'none';
+							oMoodSad.style.display = 'block';
+							break;
+						case 'confirm-yes':
+							oMoodMessage.innerHTML = "做到最好只为博得君子莞尔一笑" + foodNameTxt + "此时正在犄角旮旯歇斯底里的狂笑并享受被君子翻牌的自豪感和即将被食用的快乐感。";
+							oWrapperInner.style.display = 'none';
+							oConfirm.style.display = 'none';
+							oMoodHappy.style.display = 'block';
+							oFoodName.innerHTML = foodNameTxt;
+							break;
+					}
+				}, true);
 
-			//停止事件
-			oStop.onclick = function() {
-				for (var i = 0, len = foodBtnObj.length; i < len; i++) {
-					oBtns[i].className = 'sim-button button';
+				oMoodBtn[0].onclick = oMoodBtn[1].onclick = function() {
+					oWrapperInner.style.display = 'block';
+					oConfirm.style.display = 'none';
+					oMoodSad.style.display = 'none';
+					oMoodHappy.style.display = 'none';
 				}
-				window.clearInterval(timer);
-				flag = true;
-				var index = (parseInt(oFood.style.top) / 45 - 1).toString().slice(1);
-				foodNameTxt = oLi[index - 1].innerHTML;
-				oConfirmMsginnerHTML = '咱今天就吃"' + foodNameTxt + '"???';
-				oWrapperInner.style.display = 'none';
-				oConfirm.style.display = 'block';
-				oFoodName2.innerHTML = foodNameTxt;
-			};
-			oConfirmNo.onclick = function() {
-				oMoodMessage.innerHTML = foodNameTxt + " 已黯然失色躲在角落检测身体营养指数哭到妆都花了还是会深思熟虑如何才能博君子一笑即使还是会痛定思痛。";
-				oWrapperInner.style.display = 'none';
-				oConfirm.style.display = 'none';
-				oMoodSad.style.display = 'block'; 
-			}
-			oConfirmYes.onclick = function() {
-				oMoodMessage.innerHTML = "做到最好只为博得君子莞尔一笑" + foodNameTxt + "此时正在犄角旮旯歇斯底里的狂笑并享受被君子翻牌的自豪感和即将被食用的快乐感。";
-				oWrapperInner.style.display = 'none';
-				oConfirm.style.display = 'none';
-				oMoodHappy.style.display = 'block';
-				oFoodName.innerHTML = foodNameTxt;
-			}
-			oMoodBtn[0].onclick = oMoodBtn[1].onclick = function() {
-				oWrapperInner.style.display = 'block';
-				oConfirm.style.display = 'none';
-				oMoodSad.style.display = 'none';
-				oMoodHappy.style.display = 'none';
 			}
 		}
 	}
-}
-loadFood();
-
-
+	loadFood();
--- js/modifyFood.js
+++ js/modifyFood.js
@@ -1,6 +1,18 @@
 var name, type, newname, newtype; //食物名称 食物类型
-var oTypeUl = document.getElementById('type-ul');
-var oTypeUl2 = document.getElementById('type-ul2');
+var getId = (function(func) {
+	return function() {
+		return func.apply(document, arguments);
+	}
+})(document.getElementById);
+
+var getClass = (function(func) {
+	return function() {
+		return func.apply(document, arguments);
+	}
+})(document.getElementsByClassName);
+
+var oTypeUl = getId('type-ul');
+var oTypeUl2 = getId('type-ul2');
 var typeFood = [{
 	name: "Breakfast",
 	type: 1
@@ -12,14 +24,14 @@ var typeFood = [{
 	type: 3
 }];
 
-document.getElementById('type-tips').onclick = function() {
+getId('type-tips').onclick = function() {
 	oTypeUl.style.display = "block";
 }
-var oLi = document.getElementById('type-ul').children;
+var oLi = getId('type-ul').children;
 for (var i = 0, len = oLi.length; i < len; i++) {
 	oLi[i].onclick = function() {
 		var typeName = this.innerHTML;
-		document.getElementById('type-tips').innerHTML = typeName;
+		getId('type-tips').innerHTML = typeName;
 		for (var j in typeFood) {
 			typeFood[j].name == typeName ? type = ++j : type == 0;
 		}
@@ -27,11 +39,11 @@ for (var i = 0, len = oLi.length; i < len; i++) {
 	}
 } 
 function successSkip(txt) {
-	document.getElementsByClassName('success-box')[0].style.zIndex = "block"; 
-	document.getElementsByClassName('success-box')[0].innerHTML = txt;  
-	var opacityVal = eval(document.getElementsByClassName('success-box')[0].getAttribute('style')); 
-	document.getElementById('loading').style.display = 'none';
-	document.getElementsByClassName('success-box')[0].style.opacity = 1;
+	getClass('success-box')[0].style.zIndex = "block"; 
+	getClass('success-box')[0].innerHTML = txt;  
+	var opacityVal = eval(getClass('success-box')[0].getAttribute('style')); 
+	getId('loading').style.display = 'none';
+	getClass('success-box')[0].style.opacity = 1;
 	setTimeout('window.location.href = "index.html";', 1200);
 }
  
